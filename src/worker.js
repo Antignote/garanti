abe48ec53e884867770e8278f9a097acf57b6213
hornta <hornta@protonmail.com>
2021-03-06 02:34:35 +0100

    fix workes

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,13 @@
 export const garantiWorker = new Worker(
   new URL('./garanti.js', import.meta.url),
   {
-    type: import.meta.env.MODE === 'development' ? 'module' : 'classic',
+    type: 'module',
   },
 );
 
 export const tableWorker = new Worker(
   new URL('./table-worker.js', import.meta.url),
   {
-    type: import.meta.env.MODE === 'development' ? 'module' : 'classic',
+    type: 'module',
   },
 );
